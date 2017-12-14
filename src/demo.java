582adeb0bfd6a4bfd6496cc683bc080a323dab98
goodsen <18618411965@163.com>
2017-12-14 22:14:57 +0800

    Initial commit

---
+++ src/demo.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class demo {
+    public static void main(String[] args) {
+        //Todo Auto-generated method stub
+        WebDriver driver;
+        driver = new FirefoxDriver();
+        driver.get("http://www.baidu.com");
+    }
+}
