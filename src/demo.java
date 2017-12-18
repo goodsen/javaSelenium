7bf0f6ca20141d0edebca17e09a1aefb88a29fea
goodsen <18618411965@163.com>
2017-12-18 21:57:51 +0800

    增加打开登录页面元素定位

--- src/demo.java
+++ src/demo.java
@@ -1,11 +1,14 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
 
 public class demo {
     public static void main(String[] args) {
         //Todo Auto-generated method stub
-        WebDriver driver;
-        driver = new FirefoxDriver();
-        driver.get("http://www.baidu.com");
-    }
+        WebDriver driver; //定义Driver
+        driver = new FirefoxDriver();//打开火狐浏览器
+        driver.get("http://www.casicloud.com");//打开云网首页
+        driver.findElement(By.xpath(".//*[@id='login']")).click();//打开登录页面
+        driver.quit();//浏览器关闭
+        }
 }
